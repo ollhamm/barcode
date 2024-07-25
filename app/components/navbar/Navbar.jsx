e50ba26da79c081d2878240144de2b4e24581290
ollhamm <ilhamkusmayadi65@gmail.com>
2024-07-25 20:07:46 +0700

    update nav

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -10,14 +10,14 @@ const Navbar = () => {
       onClick={() => router.push("/")}
       className="w-full flex flex-row items-start p-8 py-3 px-6  bg-[#9dc98a]"
     >
-      <div className="flex cursor-pointer flex-row items-center justify-start text-neutral-600 md:justify-center gap-6 font-semibold ml-4 md:ml-0">
+      <div className="flex cursor-pointer flex-row items-center text-xs md:text-xl justify-start text-neutral-600 md:justify-center gap-6 font-semibold ml-4 md:ml-0">
         <Image
           src={"/images/logo-amat.png"}
           alt="logo"
           height={40}
           width={40}
         />
-        KertaAji
+        Keluarga Taruna Muda Jitar Dukuh
       </div>
     </div>
   );
